481c5f352a8fe40a2e865d01ef511dd8d409d027
azureus-jim <wen.yanzhe@gmail.com>
2021-08-24 14:22:23 +0800

    Added ETA column to the GUI app.

--- main_psg.py
+++ main_psg.py
@@ -6,7 +6,11 @@ from weather_vault import Collector
 def one_dbTable(window, look_for, db_table_name, build_limit):
     scrapper = Collector(look_for=look_for, build_format='V1', ping_interval=60)
     for cnt in scrapper.build_df(db_table_name=db_table_name, limit=build_limit, send_to_db=True): # There are yield calls in the build_df function, with generates and iterative-like object that's used to update the ProgressBar
-        window[f'-PB_{look_for}-'].UpdateBar(cnt, build_limit)          
+        window[f'-PB_{look_for}-'].UpdateBar(cnt, build_limit)
+        if cnt != build_limit:
+            window[f'-ETA_{look_for}-'].Update(f"< {int((build_limit-cnt)*5)}:00")
+        else:
+            window[f'-ETA_{look_for}-'].Update("Completed!")        
     window.write_event_value('-THREAD DONE-', f'{db_table_name}')
 
 # Function to execute a thread for collecting a particular data type in the background (daemon)
@@ -40,22 +44,22 @@ sg.theme('My New Theme')
 
 nopad = ((0, 0), (0, 0))        # ((left, right), (top, bottom))
 header_layout = [
-    [sg.Text(text="Data Types", size=(25, 1), background_color='orange', justification='center', pad=nopad, font=('Verdana', 10, ['bold'])), sg.Text(text="Entries", size=(8, 1), background_color='orange', justification='center', pad=nopad, font=('Verdana', 10, ['bold'])), sg.Text(text="Collection Progress", size=(63, 1), background_color='orange', pad=nopad, justification='center', font=('Verdana', 10, ['bold'])), sg.Text(text="Execute", size=(10, 1), background_color='yellow', pad=nopad, expand_x=True, justification='center', font=('Verdana', 10, ['bold']))]
+    [sg.Text(text="Data Types", size=(25, 1), background_color='orange', justification='center', pad=nopad, font=('Verdana', 10, ['bold'])), sg.Text(text="Entries", size=(8, 1), background_color='orange', justification='center', pad=nopad, font=('Verdana', 10, ['bold'])), sg.Text(text="Collection Progress", size=(63, 1), background_color='orange', pad=nopad, justification='center', font=('Verdana', 10, ['bold'])), sg.Text(text="Execute", size=(10, 1), background_color='yellow', pad=nopad, justification='center', font=('Verdana', 10, ['bold'])), sg.Text(text="ETA", size=(10, 1), background_color='#78FF00', pad=nopad, expand_x=True, justification='center', font=('Verdana', 10, ['bold']))]
 ]
 
 
 col_layout = [
         [sg.Frame(title='', layout=header_layout)],
-        [sg.Text(text="Air Temperature:", size=(28, 1), pad=((10, 0), 0)), sg.Input(size=(8, 1), key='-num_entries_temp-'), sg.ProgressBar(288, orientation='h', size=(50, 20), pad=(15, 0), key='-PB_temp-'), sg.Button(button_text="Collect", size=(9, 1), key="-collect_data_temp-", pad=((0, 5), 2), border_width=2)],
-        [sg.Text(text="Rainfall:", size=(28, 1), pad=((10, 0), 0)), sg.Input(size=(8, 1), key='-num_entries_rain-'), sg.ProgressBar(288, orientation='h', size=(50, 20), pad=(15, 0), key='-PB_rain-'), sg.Button(button_text="Collect", size=(9, 1), key="-collect_data_rain-", pad=((0, 5), 2), border_width=2)],
-        [sg.Text(text="Relative Humidity:", size=(28, 1), pad=((10, 0), 0)), sg.Input(size=(8, 1), key='-num_entries_humid-'), sg.ProgressBar(288, orientation='h', size=(50, 20), pad=(15, 0), key='-PB_humid-'), sg.Button(button_text="Collect", size=(9, 1), key="-collect_data_humid-", pad=((0, 5), 2), border_width=2)],
-        [sg.Text(text="Wind Direction:", size=(28, 1), pad=((10, 0), 0)), sg.Input(size=(8, 1), key='-num_entries_direction-'), sg.ProgressBar(288, orientation='h', size=(50, 20), pad=(15, 0), key='-PB_direction-'), sg.Button(button_text="Collect", size=(9, 1), key="-collect_data_direction-", pad=((0, 5), 2), border_width=2)],
-        [sg.Text(text="Wind Speed:", size=(28, 1), pad=((10, 0), 0)), sg.Input(size=(8, 1), key='-num_entries_speed-'), sg.ProgressBar(288, orientation='h', size=(50, 20), pad=(15, 0), key='-PB_speed-'), sg.Button(button_text="Collect", size=(9, 1), key="-collect_data_speed-", pad=((0, 5), 2), border_width=2)]
+        [sg.Text(text="Air Temperature:", size=(28, 1), pad=((10, 0), 0)), sg.Input(size=(8, 1), key='-num_entries_temp-'), sg.ProgressBar(288, orientation='h', size=(50, 20), pad=(15, 0), key='-PB_temp-'), sg.Button(button_text="Collect", size=(9, 1), key="-collect_data_temp-", pad=((0, 5), 2), border_width=2), sg.Text(text="", size=(9, 1), font=('Verdana', 10, ['italic']), pad=((8, 0), 0), justification='center', key='-ETA_temp-')],
+        [sg.Text(text="Rainfall:", size=(28, 1), pad=((10, 0), 0)), sg.Input(size=(8, 1), key='-num_entries_rain-'), sg.ProgressBar(288, orientation='h', size=(50, 20), pad=(15, 0), key='-PB_rain-'), sg.Button(button_text="Collect", size=(9, 1), key="-collect_data_rain-", pad=((0, 5), 2), border_width=2), sg.Text(text="", size=(9, 1), font=('Verdana', 10, ['italic']), pad=((8, 0), 0), justification='center', key='-ETA_rain-')],
+        [sg.Text(text="Relative Humidity:", size=(28, 1), pad=((10, 0), 0)), sg.Input(size=(8, 1), key='-num_entries_humid-'), sg.ProgressBar(288, orientation='h', size=(50, 20), pad=(15, 0), key='-PB_humid-'), sg.Button(button_text="Collect", size=(9, 1), key="-collect_data_humid-", pad=((0, 5), 2), border_width=2), sg.Text(text="", size=(9, 1), font=('Verdana', 10, ['italic']), pad=((8, 0), 0), justification='center', key='-ETA_humid-')],
+        [sg.Text(text="Wind Direction:", size=(28, 1), pad=((10, 0), 0)), sg.Input(size=(8, 1), key='-num_entries_direction-'), sg.ProgressBar(288, orientation='h', size=(50, 20), pad=(15, 0), key='-PB_direction-'), sg.Button(button_text="Collect", size=(9, 1), key="-collect_data_direction-", pad=((0, 5), 2), border_width=2), sg.Text(text="", size=(9, 1), font=('Verdana', 10, ['italic']), pad=((8, 0), 0), justification='center', key='-ETA_direction-')],
+        [sg.Text(text="Wind Speed:", size=(28, 1), pad=((10, 0), 0)), sg.Input(size=(8, 1), key='-num_entries_speed-'), sg.ProgressBar(288, orientation='h', size=(50, 20), pad=(15, 0), key='-PB_speed-'), sg.Button(button_text="Collect", size=(9, 1), key="-collect_data_speed-", pad=((0, 5), 2), border_width=2), sg.Text(text="", size=(9, 1), font=('Verdana', 10, ['italic']), pad=((8, 0), 0), justification='center', key='-ETA_speed-')]
         ]
 
 layout = [[sg.Text(text="Data Collector Controller for SGWeather", font=('Garamond', 14, ['bold', 'italic']))],
           [sg.Col(col_layout, pad=(0, 0))],
-          [sg.Multiline(size=(135, 20), reroute_stdout=True, do_not_clear=False, autoscroll=True, write_only=True, key='-console-')],
+          [sg.Multiline(size=(140, 20), reroute_stdout=True, do_not_clear=False, autoscroll=True, write_only=True, expand_x=True, key='-console-')],
           [sg.Button(button_text="Clear output", key="-clear-")]]
 
 window = sg.Window("SGWeather - Data Collector", layout, finalize=True)
